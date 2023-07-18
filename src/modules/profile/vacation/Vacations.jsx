283eb4ecfe4c66225d2e405f813fc8633a0f42fd
nguyencongson96 <chansen2912@gmail.com>
2023-07-18 21:27:50 +0700

    update profile

--- src/modules/profile/vacation/Vacations.jsx
+++ src/modules/profile/vacation/Vacations.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames/bind";
 import styles from "./Vacation.module.scss";
 import { HeartFilled, CommentOutlined, EyeOutlined } from "@ant-design/icons";
 import { getListVacation } from "~/store/slices/vacationSlice";
-import { Card, List, Typography, Skeleton, Empty } from "antd";
+import { Card, List, Typography, Skeleton } from "antd";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
@@ -27,65 +27,52 @@ const Vacations = () => {
 
   return (
     <div className={cx("feed-container")}>
-      {list.length > 0 ? (
-        <InfiniteScroll
-          // scrollThreshold="10%"
-          dataLength={list.length}
-          next={loadMoreData}
-          hasMore={page < pages}
-          loader={
-            <Skeleton
-              avatar
-              paragraph={{
-                rows: 1,
-              }}
-              active
-            />
-          }
-        >
-          <List
-            className={cx("feed")}
-            grid={{ gutter: 30, column: 3 }}
-            dataSource={list}
-            renderItem={(item) => (
-              <List.Item className={cx("feed-item")}>
-                <NavLink to={`/vacation?vacationID=${item._id}`}>
-                  <Card
-                    bordered={false}
-                    className={cx("feed-card")}
-                    hoverable={true}
-                    cover={<img className={cx("feed-cover")} src={item?.cover?.path} alt="" />}
+      <InfiniteScroll
+        dataLength={list.length}
+        next={loadMoreData}
+        hasMore={page < pages}
+        loader={<Skeleton paragraph={{ rows: 1 }} active />}
+      >
+        <List
+          className={cx("feed")}
+          grid={{ gutter: 30, column: 3 }}
+          dataSource={list}
+          renderItem={(item) => (
+            <List.Item className={cx("feed-item")}>
+              <NavLink to={`/vacation?vacationID=${item._id}`}>
+                <Card
+                  bordered={false}
+                  className={cx("feed-card")}
+                  hoverable={true}
+                  cover={<img className={cx("feed-cover")} src={item?.cover?.path} alt="" />}
+                >
+                  <Typography.Paragraph
+                    className={cx("feed-title")}
+                    ellipsis={{ expandable: false, rows: 1 }}
                   >
-                    <Typography.Paragraph
-                      className={cx("feed-title")}
-                      ellipsis={{ expandable: false, rows: 1 }}
-                    >
-                      {item.title}
-                    </Typography.Paragraph>
-                    <div className={cx("feed-cover-rad")}></div>
-                    <div className={cx("feed-info")}>
-                      <div className={cx("views")}>
-                        <EyeOutlined />
-                        {formatter.format(item.views)}
-                      </div>
-                      <div className={cx("likes")}>
-                        <HeartFilled />
-                        {formatter.format(item.likes)}
-                      </div>
-                      <div className={cx("cmts")}>
-                        <CommentOutlined />
-                        {formatter.format(item.comments)}
-                      </div>
+                    {item.title}
+                  </Typography.Paragraph>
+                  <div className={cx("feed-cover-rad")}></div>
+                  <div className={cx("feed-info")}>
+                    <div className={cx("views")}>
+                      <EyeOutlined />
+                      {formatter.format(item.views)}
                     </div>
-                  </Card>
-                </NavLink>
-              </List.Item>
-            )}
-          />
-        </InfiniteScroll>
-      ) : (
-        <Empty description={false} />
-      )}
+                    <div className={cx("likes")}>
+                      <HeartFilled />
+                      {formatter.format(item.likes)}
+                    </div>
+                    <div className={cx("cmts")}>
+                      <CommentOutlined />
+                      {formatter.format(item.comments)}
+                    </div>
+                  </div>
+                </Card>
+              </NavLink>
+            </List.Item>
+          )}
+        />
+      </InfiniteScroll>
     </div>
   );
 };

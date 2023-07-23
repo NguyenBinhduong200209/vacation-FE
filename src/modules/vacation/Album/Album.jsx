0118040a61c1898879ec971c41687f028bd0e414
hieu.ltt <hieultt.223@gmail.com>
2023-07-23 13:29:38 +0700

    album

--- src/modules/vacation/Album/Album.jsx
+++ src/modules/vacation/Album/Album.jsx
@@ -15,7 +15,7 @@ const Album = () => {
     const fetchData = async () => {
       try {
         const fetchImg = await axiosClient.get(
-          `vacation/${dataId.vacationID}/images`
+          `vacation/${dataId.id}/images` // Fix the dataId object key here
         );
         setImg(fetchImg.data.data);
         console.log(fetchImg);
@@ -26,22 +26,25 @@ const Album = () => {
 
     fetchData();
   }, [dataId.id]);
-  console.log(dataId.vacationID);
 
   return (
     <div className={cx("album-wrap")}>
       <div className={cx("album-title")}>Vacation's Album</div>
-      <div className={cx("album-container")}>
-        {img.map((item, index) => (
-          <div className={cx("stack")}>
-            <div className={cx("card-album-container")} key={index}>
-              <div className={cx("image")}>
-                <img src={item?.path} alt="?" className={cx("images")} />
+      {img.length === 0 ? (
+        <div className={cx("no-images")}>Không có ảnh</div>
+      ) : (
+        <div className={cx("album-container")}>
+          {img.map((item, index) => (
+            <div className={cx("stack")} key={index}>
+              <div className={cx("card-album-container")}>
+                <div className={cx("image")}>
+                  <img src={item?.path} alt="?" className={cx("images")} />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };

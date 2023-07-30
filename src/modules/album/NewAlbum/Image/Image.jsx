1c6b67e0d0128a547ff54839ca56f22b32078e62
dinhbaokhanh <khanhdbao@gmail.com>
2023-07-30 17:38:43 +0700

    update

--- src/modules/album/NewAlbum/Image/Image.jsx
+++ src/modules/album/NewAlbum/Image/Image.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "react-resizable/css/styles.css";
-import Draggable from "react-draggable";
+import DraggableCore from "react-draggable";
 import { useDispatch } from "react-redux";
 import { removeSelected, updateSelected } from "~/store/slices/albumSlice";
 import { ResizableBox } from "react-resizable";
@@ -10,66 +10,68 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 const Image = ({ imgData, containerSize }) => {
-  const {
-    style: { width, height, left, top },
-    _id,
-    path,
-  } = imgData;
-  const dispatch = useDispatch();
-  const handleResize = (event, { size }) => {
-    dispatch(
-      updateSelected({
-        _id,
-        path,
-        style: { width: size.width, height: size.height, top, left },
-      })
-    );
-  };
+	const {
+		style: { width, height, left, top },
+		_id,
+		path,
+	} = imgData;
+	const dispatch = useDispatch();
+	const handleResize = (event, { size }) => {
+		dispatch(
+			updateSelected({
+				_id,
+				path,
+				style: { width: size.width, height: size.height, top, left },
+			})
+		);
+	};
 
-  const handleDragStop = (event, data) => {
-    const { lastX, lastY } = data;
-    event.target.parentElement &&
-      dispatch(
-        updateSelected({
-          _id,
-          path,
-          style: { width, height, top: lastY, left: lastX },
-        })
-      );
-  };
+	const handleDragStop = (event, data) => {
+		const { lastX, lastY } = data;
+		event.target.parentElement &&
+			dispatch(
+				updateSelected({
+					_id,
+					path,
+					style: { width, height, top: lastY, left: lastX },
+				})
+			);
+	};
 
-  const handleImageRemove = () => {
-    dispatch(removeSelected(_id));
-  };
+	const handleImageRemove = () => {
+		dispatch(removeSelected(_id));
+	};
 
-  return (
-    <Draggable
-      handle={`.${cx("handle")}`}
-      defaultPosition={{ x: left, y: top }}
-      onStop={handleDragStop}
-      bounds="parent"
-      defaultClassName={cx("draggable")}
-    >
-      <ResizableBox
-        width={width}
-        height={height}
-        onResizeStop={handleResize}
-        minConstraints={[50, 50]}
-        maxConstraints={[containerSize.outerWidth - left, containerSize.outerHeight - top]}
-      >
-        <div
-          className={cx("handle")}
-          style={{
-            backgroundImage: `url(${path})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <CloseCircleOutlined className={cx("close-icon")} onClick={handleImageRemove} />
-        </div>
-      </ResizableBox>
-    </Draggable>
-  );
+	return (
+		<DraggableCore
+			handle={`.${cx("handle")}`}
+			defaultPosition={{ x: left, y: top }}
+			onStop={handleDragStop}
+			bounds="parent"
+			defaultClassName={cx("draggable")}
+		>
+			<div>
+				<ResizableBox
+					width={width}
+					height={height}
+					onResizeStop={handleResize}
+					minConstraints={[50, 50]}
+					maxConstraints={[containerSize.outerWidth - left, containerSize.outerHeight - top]}
+				>
+					<div
+						className={cx("handle")}
+						style={{
+							backgroundImage: `url(${path})`,
+							backgroundSize: "cover",
+							backgroundPosition: "center",
+						}}
+					>
+						<CloseCircleOutlined className={cx("close-icon")} onClick={handleImageRemove} />
+					</div>
+				</ResizableBox>
+			</div>
+		</DraggableCore>
+	);
 };
 
 export default Image;
